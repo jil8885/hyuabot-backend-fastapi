f86a82c152518e311f49d3ee767868d33f375b44
Jeongin Lee <jil8885@hanyang.ac.kr>
2023-05-07 17:59:37 +0900

    test: 열람실 좌석 쿼리 테스트 개선

--- tests/test_library_query.py
+++ tests/test_library_query.py
@@ -7,6 +7,52 @@ from app.main import app
 
 
 @pytest.mark.asyncio
+async def test_reading_room_query():
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.post(
+            "/query",
+            json={
+                "query": """
+                    query {
+                        readingRoom {
+                            campusId
+                            id
+                            name
+                            status {
+                                active
+                                reservable
+                            }
+                            seats {
+                                total
+                                active
+                                occupied
+                                available
+                            }
+                            updatedAt
+                        }
+                    }
+                """,
+            },
+        )
+        assert response.status_code == 200
+        response_body = response.json()
+        assert type(response_body["data"]["readingRoom"]) == list
+        for reading_room in response_body["data"]["readingRoom"]:
+            assert type(reading_room["id"]) == int
+            assert type(reading_room["name"]) == str
+            assert type(reading_room["status"]["active"]) == bool
+            assert type(reading_room["status"]["reservable"]) == bool
+            assert type(reading_room["seats"]["total"]) == int
+            assert type(reading_room["seats"]["active"]) == int
+            assert type(reading_room["seats"]["occupied"]) == int
+            assert type(reading_room["seats"]["available"]) == int
+            assert type(reading_room["updatedAt"]) == str
+            assert reading_room["seats"]["active"] == \
+                   reading_room["seats"]["occupied"] + \
+                   reading_room["seats"]["available"]
+
+
+@pytest.mark.asyncio
 async def test_reading_room_query_by_campus():
     campus_list = [1, 2]
     async with AsyncClient(app=app, base_url="http://test") as client:
@@ -93,6 +139,51 @@ async def test_reading_room_query_by_room_list():
             assert reading_room["id"] in room_list
             assert type(reading_room["name"]) == str
             assert type(reading_room["status"]["active"]) == bool
+            assert type(reading_room["status"]["reservable"]) == bool
+            assert type(reading_room["seats"]["total"]) == int
+            assert type(reading_room["seats"]["active"]) == int
+            assert type(reading_room["seats"]["occupied"]) == int
+            assert type(reading_room["seats"]["available"]) == int
+            assert type(reading_room["updatedAt"]) == str
+            assert reading_room["seats"]["active"] == \
+                   reading_room["seats"]["occupied"] + \
+                   reading_room["seats"]["available"]
+
+
+@pytest.mark.asyncio
+async def test_reading_room_query_by_active_value():
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.post(
+            "/query",
+            json={
+                "query": """
+                    query {
+                        readingRoom(active: true) {
+                            id
+                            name
+                            status {
+                                active
+                                reservable
+                            }
+                            seats {
+                                total
+                                active
+                                occupied
+                                available
+                            }
+                            updatedAt
+                        }
+                    }
+                """,
+            },
+        )
+        assert response.status_code == 200
+        response_body = response.json()
+        assert type(response_body["data"]["readingRoom"]) == list
+        for reading_room in response_body["data"]["readingRoom"]:
+            assert type(reading_room["name"]) == str
+            assert type(reading_room["status"]["active"]) == bool
+            assert reading_room["status"]["active"] is True
             assert type(reading_room["status"]["reservable"]) == bool
             assert type(reading_room["seats"]["total"]) == int
             assert type(reading_room["seats"]["active"]) == int
